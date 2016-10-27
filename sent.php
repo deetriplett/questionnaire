93613d1109911b8659546a62b3796c0fa090c7f4
deetriplett <deetriplett@gmail.com>
2016-10-27 09:44:41 -0400

    Create sent.php

---
+++ sent.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = "Thank You";
+$section = null;
+include("inc/header.php");
+?>
+
+<div class="section-page">
+  <div class="container-fluid thanks">
+  <h1>Thank You!</h1>
+  <p>Your Questionnaire has been sent</p>
+</div>
+
+<?php include('inc/footer.php');?>
